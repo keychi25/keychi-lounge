e676f53f753cb64497bc781e075bac6f9b7291dd
keychi25 <m208125n@st.u-gkugei.ac.jp>
2021-10-07 15:17:19 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps}>
+      <ChakraProvider />
+    </Component>
+  );
 }
-export default MyApp
+export default MyApp;

18eadaffc74e263ac1f9918e03ce771976f26b69
keychi25 <m208125n@st.u-gkugei.ac.jp>
2021-10-11 12:52:48 +0900

    add : wors-base

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import { Divider } from "@chakra-ui/layout";
 import type { NextPage } from "next";
 import SiteDescription from "../component/SiteDescription";
 import MyProfile from "../component/MyProfile";
+import Work from "../component/Work";
 const Home: NextPage = () => {
   return (
     <>
       <SiteDescription />
       <Divider />
       <MyProfile />
+      <Divider />
+      <Work />
     </>
   );
 };

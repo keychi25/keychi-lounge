314cee2ff87d97eee710522c3680ad098e004d5b
keychi25 <e165412y@st.u-gakugei.ac.jp>
2021-11-01 07:57:37 +0900

    add: pagenation-base

--- component/SkillTable.tsx
+++ component/SkillTable.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Table, Thead, Tbody, Tr, Th, Td, chakra } from "@chakra-ui/react";
 import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
-import { useTable, useSortBy, Column } from "react-table";
+import { useTable, useSortBy, Column, usePagination } from "react-table";
 
 export type DataTableProps<Data extends object> = {
   data: Data[];
@@ -12,53 +12,125 @@ export default function SkillTable<Data extends object>({
   data,
   columns,
 }: DataTableProps<Data>) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data }, useSortBy);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    page,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    { columns, data, initialState: { pageIndex: 1, pageSize: 5 } },
+    useSortBy,
+    usePagination
+  );
 
   return (
-    <Table {...getTableProps()}>
-      <Thead>
-        {headerGroups.map((headerGroup: any) => (
-          <Tr key={headerGroup.headers} {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column: any) => (
-              <Th
-                key={column.accessor}
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-                isNumeric={column.isNumeric}
-              >
-                {column.render("Header")}
-                <chakra.span pl="4">
-                  {column.isSorted ? (
-                    column.isSortedDesc ? (
-                      <TriangleDownIcon aria-label="sorted descending" />
-                    ) : (
-                      <TriangleUpIcon aria-label="sorted ascending" />
-                    )
-                  ) : null}
-                </chakra.span>
-              </Th>
-            ))}
-          </Tr>
-        ))}
-      </Thead>
-      <Tbody {...getTableBodyProps()}>
-        {rows.map((row: any) => {
-          prepareRow(row);
-          return (
-            <Tr key={row.name} {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <Td
-                  key={cell.column.accessor}
-                  {...cell.getCellProps()}
-                  isNumeric={cell.column.isNumeric}
+    <>
+      <Table {...getTableProps()}>
+        <Thead>
+          {headerGroups.map((headerGroup: any) => (
+            <Tr
+              key={headerGroup.headers}
+              {...headerGroup.getHeaderGroupProps()}
+            >
+              {headerGroup.headers.map((column: any) => (
+                <Th
+                  key={column.accessor}
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  isNumeric={column.isNumeric}
                 >
-                  {cell.render("Cell")}
-                </Td>
+                  {column.render("Header")}
+                  <chakra.span pl="4">
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
               ))}
             </Tr>
-          );
-        })}
-      </Tbody>
-    </Table>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {rows.map((row: any) => {
+            prepareRow(row);
+            return (
+              <Tr key={row.name} {...row.getRowProps()}>
+                {row.cells.map((cell: any) => (
+                  <Td
+                    key={cell.column.accessor}
+                    {...cell.getCellProps()}
+                    isNumeric={cell.column.isNumeric}
+                  >
+                    {cell.render("Cell")}
+                  </Td>
+                ))}
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+      <ul className="pagination">
+        <li className="page-item" onClick={() => gotoPage(0)}>
+          <a className="page-link">First</a>
+        </li>
+        <li className="page-item" onClick={() => previousPage()}>
+          <a className="page-link">{"<"}</a>
+        </li>
+        <li className="page-item" onClick={() => nextPage()}>
+          <a className="page-link">{">"}</a>
+        </li>
+        <li className="page-item" onClick={() => gotoPage(pageCount - 1)}>
+          <a className="page-link">Last</a>
+        </li>
+        <li>
+          <a className="page-link">
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </a>
+        </li>
+        <li>
+          <a className="page-link">
+            <input
+              className="form-control"
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                gotoPage(page);
+              }}
+              style={{ width: "100px", height: "20px" }}
+            />
+          </a>
+        </li>{" "}
+        <select
+          className="form-control"
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+          style={{ width: "120px", height: "38px" }}
+        >
+          {[5, 10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </ul>
+    </>
   );
 }
